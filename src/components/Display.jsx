6fe38c5d3c1d379766ddc6d612f52460c4049618
Kameswar Panda <kameswarpanda11@gmail.com>
2024-03-04 12:28:25 +0530

    Initial commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Display.module.css'
+
+export default function Display() {
+  return (
+    <div>
+      <input className={styles.display} type="text" />
+    </div>
+  )
+}
